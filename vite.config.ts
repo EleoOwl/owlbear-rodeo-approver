dd73bad1adeac718b4bcfde2d4af2a5406601386
EleoOwl <eleo.lebedeva.v@gmail.com>
2025-06-11 01:17:04 +0300

    Updated vite configs to include non-index html pages for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { resolve } from "path";
+
+declare var __dirname: string;
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         cors: {
             origin: "https://www.owlbear.rodeo",
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                popover: resolve(__dirname, "popover.html"),
+                background: resolve(__dirname, "background.html"),
+            },
         },
     },
 })
